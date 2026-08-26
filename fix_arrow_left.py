d35f6343d9afe8cd63e535f8a4578383af136d5a
mehmetsensoy <mehmetsensoyme@gmail.com>
2026-08-27 02:12:09 +0300

    chore: clean up temporary patch and generator scripts

--- fix_arrow_left.py
+++
@@ -1,9 +0,0 @@
-with open('src/pages/Login.tsx', 'r') as f:
-    code = f.read()
-
-# Add ArrowLeft to imports if it's missing
-if 'ArrowLeft' not in code.split('from \'lucide-react\'')[0]:
-    code = code.replace("Phone }", "Phone, ArrowLeft }")
-
-with open('src/pages/Login.tsx', 'w') as f:
-    f.write(code)
